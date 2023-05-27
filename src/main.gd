c2876e932870f2b6715ae4d3ccb4923dd62bc7ea
Harish <dev.harish3124@gmail.com>
2023-05-28 00:11:43 +0530

    Fix Bugs

--- src/main.gd
+++ src/main.gd
@@ -4,6 +4,8 @@ var Globals = preload("res://src/globals/global_resource.tres")
 
 var mouse_button_down: bool = false
 
+var map_template: Array
+
 func _ready():
 	var tmp = []
 	tmp.resize(64)
@@ -11,14 +13,21 @@ func _ready():
 
 	for i in range(32):
 		Globals.map.append(tmp.duplicate())
-
+	
+	map_template = Globals.map.duplicate(true)
 
 func _process(_delta):
+	if Globals.run:
+		var next_gen_map = map_template.duplicate(true)
+
+		for row in range(len(Globals.map)):
+			for col in range(len(Globals.map[row])):
+				simulate(next_gen_map, row, col)
+		
+		Globals.map =  next_gen_map
 
 	for row in range(len(Globals.map)):
 		for col in range(len(Globals.map[row])):
-			if Globals.run:
-				simulate(row, col)
 			var cell_type = Globals.map[row][col]
 			set_cell(
 			0,                     #Layer ID (0 by default)
@@ -52,20 +61,20 @@ func _input(event):
 			else:
 				Globals.map[pos.y][pos.x] = 0
 
-func simulate(row, col):
+func simulate(next_gen_map, row, col):
 	var neighbor_count = 0 
 	for e in [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1], [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]:
 		if (e[0] >= 0 && e[0] < 32) && (e[1] >= 0 && e[1] < 64):
 			if Globals.map[e[0]][e[1]] == 1:
 				neighbor_count += 1
 
-	if neighbor_count == 0:
-		Globals.map[row][col] = 0 
+	if neighbor_count < 2:
+		next_gen_map[row][col] = 0 
 	elif neighbor_count < 3:
-		pass
+		next_gen_map[row][col] = Globals.map[row][col] 
 	elif neighbor_count == 3:
-		Globals.map[row][col] = 1 
+		next_gen_map[row][col] = 1 
 	elif neighbor_count > 3:
-		Globals.map[row][col] = 0 
+		next_gen_map[row][col] = 0 
 		
 
