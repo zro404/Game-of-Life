9999aa64ccf708d7ed4acb4441bccfb82b755a04
Harish <dev.harish3124@gmail.com>
2023-05-27 23:50:46 +0530

    Finish Algorithm

--- src/main.gd
+++ src/main.gd
@@ -17,6 +17,8 @@ func _process(_delta):
 
 	for row in range(len(Globals.map)):
 		for col in range(len(Globals.map[row])):
+			if Globals.run:
+				simulate(row, col)
 			var cell_type = Globals.map[row][col]
 			set_cell(
 			0,                     #Layer ID (0 by default)
@@ -49,3 +51,21 @@ func _input(event):
 				Globals.map[pos.y][pos.x] = 1
 			else:
 				Globals.map[pos.y][pos.x] = 0
+
+func simulate(row, col):
+	var neighbor_count = 0 
+	for e in [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1], [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]:
+		if (e[0] >= 0 && e[0] < 32) && (e[1] >= 0 && e[1] < 64):
+			if Globals.map[e[0]][e[1]] == 1:
+				neighbor_count += 1
+
+	if neighbor_count == 0:
+		Globals.map[row][col] = 0 
+	elif neighbor_count < 3:
+		pass
+	elif neighbor_count == 3:
+		Globals.map[row][col] = 1 
+	elif neighbor_count > 3:
+		Globals.map[row][col] = 0 
+		
+
